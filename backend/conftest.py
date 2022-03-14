b0b3f322de5cfd1e5f64f62570193fabda55fc5e
thierno <thiernoibrahimalo@esp.sn>
2022-03-14 20:24:44 +0000

    remove dependencis folder

--- backend/conftest.py
+++ backend/conftest.py
@@ -146,8 +146,7 @@ def user_token_headers(
     r = client.post("/api/token", data=login_data)
     tokens = r.json()
     a_token = tokens["access_token"]
-    headers = {"Authorization": f"Bearer {a_token}"}
-    return headers
+    return {"Authorization": f"Bearer {a_token}"}
 
 
 @pytest.fixture
@@ -163,5 +162,4 @@ def superuser_token_headers(
     r = client.post("/api/token", data=login_data)
     tokens = r.json()
     a_token = tokens["access_token"]
-    headers = {"Authorization": f"Bearer {a_token}"}
-    return headers
+    return {"Authorization": f"Bearer {a_token}"}
