2c38262ae14b284e6d7103c8b9817c72e2056abe
thierno <thiernoibrahimalo@esp.sn>
2022-03-14 23:02:59 +0000

    nice

--- backend/app/main.py
+++ backend/app/main.py
@@ -35,4 +35,4 @@ app.include_router(
 app.include_router(auth_router, prefix="/api", tags=["auth"])
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", reload=True, port=80)
+    uvicorn.run("main:app", host="0.0.0.0", reload=True, port=8000)
